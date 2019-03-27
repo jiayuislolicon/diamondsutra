fb1b99fb9c76973ecb97f44157acf801869a23a9
lolicon <lovesyndra@gmail.com>
2019-03-27 15:53:47 +0800

    web test

--- src/component/arrow.js
+++
@@ -1,4 +0,0 @@
-export const Arrow = () =>
-    <svg width="19px" height="16px" viewBox="0 0 10.2 17.3" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <path d="M8.9,16.1L1.5,8.7l7.4-7.4"/>
-    </svg>
--- src/redux/uiAction.js
+++
@@ -1,8 +0,0 @@
-export const SET_PRELOAD_DONE = 'SET_PRELOAD_DONE'
-
-export const setPreloadDone = (preloadDone) => {
-    return {
-        type: SET_PRELOAD_DONE,
-        preloadDone
-    }
-}
